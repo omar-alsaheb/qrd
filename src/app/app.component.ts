55873d4c478f196df270da871504feac28417b78
Omar AL Tamimi <OmarT@leading-point.com>
2023-05-05 13:41:06 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qrd';
+  loanAmount =17000;
+  loanYears = 5;
+  bank = 8140
+
+  res=0;
+
+  calculateLoan() { 
+  //  this.res = this.loanAmount * 0.465 / this.loanYears;
+  this.res = this.loanAmount * 0.0465 * this.loanYears;
+  this.res = this.res + this.loanAmount;
+  this.res = this.res / (this.loanYears * 12);
+
+  }
+  
 }
+
+
